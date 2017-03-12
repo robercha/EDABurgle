62a7829c9cadd3fab3c2595a252e2f63c6312134
Lucero Fernandez <lucfernandez@itba.edu.ar>
2017-03-12 15:42:21 -0300

    agregue el parser para la ip y los defines que usa

--- main.cpp
+++ main.cpp
@@ -1,15 +1,66 @@
-
-
 #include <cstdlib>
-
-
 using namespace std;
 
-/*
- *
- */
-int main(int argc, char** argv) {
-    srand(time(NULL));
+#define MAXIP 15
+#define TOTALIPDIGITS 12
+#define TOTALIPPERIODS 3
+#define IPDIGITS 3
+
+int
+main(int argc, char** argv)
+{
+    char* ip = NULL;
+    ip = parseIP(argc, argv[1]); //devuelve NULL si hubo error
+    if (ip != NULL)
+    {
+        srand(time(NULL));
+    }
+    else
+        cout << "no se pasó ip" << endl;
     return 0;
+}
+
+char*
+parseIP(int argc, char* argv)
+{
+    unsigned periodCounter = 0, digitCounter = 0, totalDigits = 0, totalPeriods = 0, i = 0, status = true;
+    if (argc == 2)
+    {
+        while (argv[i] != '\0')
+        {
+            if (isdigit(argv[i]))
+            {
+                if (digitCounter < IPDIGITS && totalDigits < TOTALIPDIGITS)
+                {
+                    digitCounter++;
+                    totalDigits++;
+                    periodCounter = 0;
+                }
+                else
+                    status = false;
+            }
+            else if (argv[0] != '.')
+            {
+                if (argv[i] == '.')
+                {
+                    if (periodCounter == 0 && totalPeriods < TOTALIPPERIODS)
+                    {
+                        periodCounter++;
+                        totalPeriods++;
+                        digitCounter = 0;
+                    }
+                }
+                else
+                    status = false;
+            }
+            else
+                status = false;
+            i++;
+        }
+    }
+    else
+        status = false;
+
+    return status == false ? NULL : argv;
 }
 
