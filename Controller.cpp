9e6dc1e9f230b7df90345e615791851a7047cb99
Lucero Fernandez <lucfernandez@itba.edu.ar>
2017-10-07 16:22:05 -0300

    comente cosas de networking

--- Controller.cpp
+++ Controller.cpp
@@ -17,52 +17,51 @@ Controller::~Controller()
     delete userData;
 }
 
-bool
-Controller::connect()
-{
-    unsigned randomNumber = rand() % (MAXTIME - MINTIME + 1);
-    networking->startTimerCount();
-    randomNumber += MINTIME;
-    bool success;
-    do
-    {
-        user->getEvent(userData);
-        success = networking->tryConnecting();
-    }
-    while ((userData->event.trueEvent != ESC) && !success && (networking->getTimerCount() <= randomNumber));
-
-    if (success)
-    {
-        printf("success connecting as client");
-    }
-    else //!success
-    {
-        printf("connection failed as client");
-        //networking->writeLog("connection failed as client");
-        do
-        {
-            user->getEvent(userData);
-            success = networking->listen();
-        }
-        while ((userData->event.trueEvent != ESC) && (userData->event.trueEvent != TIMEOUT) && !success);
-
-        if (success)
-        {
-            printf("success connecting as server");
-            //networking.writeLog("connection succeed");
-        }
-        else if (userData->event.trueEvent == ESC)
-        {
-            //networking->writeLog("Game Exited");
-        }
-        else if (userData->event.trueEvent == TIMEOUT)
-        {
-            //networking->writeLog("Timeout");
-        }
-    }
-
-    return success;
-}
+//bool Controller::connect()
+//{
+//    unsigned randomNumber = rand() % (MAXTIME - MINTIME + 1);
+//    networking->startTimerCount();
+//    randomNumber += MINTIME;
+//    bool success;
+//    do
+//    {
+//        user->getEvent(userData);
+//        success = networking->tryConnecting();
+//    }
+//    while ((userData->event.trueEvent != ESC) && !success && (networking->getTimerCount() <= randomNumber));
+//
+//    if (success)
+//    {
+//        printf("success connecting as client");
+//    }
+//    else //!success
+//    {
+//        printf("connection failed as client");
+//        //networking->writeLog("connection failed as client");
+//        do
+//        {
+//            user->getEvent(userData);
+//            success = networking->listen();
+//        }
+//        while ((userData->event.trueEvent != ESC) && (userData->event.trueEvent != TIMEOUT) && !success);
+//
+//        if (success)
+//        {
+//            printf("success connecting as server");
+//            //networking.writeLog("connection succeed");
+//        }
+//        else if (userData->event.trueEvent == ESC)
+//        {
+//            //networking->writeLog("Game Exited");
+//        }
+//        else if (userData->event.trueEvent == TIMEOUT)
+//        {
+//            //networking->writeLog("Timeout");
+//        }
+//    }
+//
+//    return success;
+//}
 
 Controller::manageEvent(void)
 {
@@ -70,24 +69,24 @@ Controller::manageEvent(void)
     translateUserData();
     FSMCycle(userData->event, gameData); //Funcion de clase FSM
 
-    networking->getEvent(userData);
-    translatePackage();
-    FSMCycle(userData->event, gameData);
+    //    networking->getEvent(userData);
+    //    translatePackage();
+    //    FSMCycle(userData->event, gameData);
 
 }
 
 Controller::initGame()
 {
-//    if (networking.getLastPacketSent == NULL) //null o noEvent, lo q sea
-//        buildPacket(NAME);
+    //    if (networking.getLastPacketSent == NULL) //null o noEvent, lo q sea
+    //        buildPacket(NAME);
     switch (userData.event.trueEvent)
     {
         case TIMEOUT:
             buildPacket(networking.getLastPacketSent);
             break;
-        case ERROR: 
+        case ERROR:
             break;
-        case QUIT: 
+        case QUIT:
             break;
         case NAME:
     }
