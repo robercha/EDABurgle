4bb3f41a4c61763440449c7a44719161aa0949c6
Lucero Fernandez <lucfernandez@itba.edu.ar>
2017-10-07 20:45:41 -0300

    cambiamos player_t por state_t

--- Player.cpp
+++ Player.cpp
@@ -4,37 +4,42 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   Player.cpp
  * Author: rober
- * 
+ *
  * Created on October 7, 2017, 4:59 PM
  */
 
 #include "Player.h"
 
-Player::Player() {
+Player::Player()
+{
 }
 
-
-
-Player::~Player() {
+Player::~Player()
+{
 }
 
-Player::eventHandler(event_t event, gameData_t gameData){
-    switch(event)
+Player::eventHandler(event_t event, gameData_t gameData)
+{
+    switch (event)
     {
-        case ACTION: sendAction(event, gameData); break; //Pasamos por parámetro el indice al arreglo de characters
-        case TURN: switchPlayer(); break;
+        case ACTION: sendAction(event, gameData);
+            break; //Pasamos por parámetro el indice al arreglo de characters
+        case TURN: switchPlayer();
+            break;
     }
 }
 
-player_t Player::getCurrentPlayer()
+state_t
+Player::getCurrentPlayer()
 {
     return currentPlayer;
 }
 
-Player::switchPlayer(){
+Player::switchPlayer()
+{
 }//dummy function
 
 Player::sendAction(event_t, gameData_t) //Llama a analizaFunction de model, aca empieza la lógica del juego.
