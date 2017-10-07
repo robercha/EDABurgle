7e7962d2f6c1f3b4ebd23b210973476c54cffcc4
Lucero Fernandez <lucfernandez@itba.edu.ar>
2017-10-07 20:20:51 -0300

    Mudamos player_t a FSM.h, analizar la generación del evento TURN para la FSM (comunicación entre Model y Controller)

--- FSM.cpp
+++ FSM.cpp
@@ -1,21 +1,22 @@
 #include "FSM.h"
 
-FSM::FSM(Player* Player1, Player* Player2){
-    Player* FSMTempMatrix[STATECOUNT][EVENTCOUNT] =  //Creamos matriz temporal para luego copiar a la final 
+FSM::FSM(Player* Player1, Player* Player2)
+{
+    Player * FSMTempMatrix[STATECOUNT][EVENTCOUNT] = //Creamos matriz temporal para luego copiar a la final
     {
         //ACTION   TURN
         {&Player1, &Player2}, //Player 1
         {&Player2, &Player1}, //Player 2
     };
-    
+
     this->FSMMatrix = new Player**[STATECOUNT]; //Alocamos las filas de la matriz
-    for(int i=0; i<STATECOUNT; i++)
+    for (int i = 0; i < STATECOUNT; i++)
     {
-        this->FSMMatrix[i]= new Player*[EVENTCOUNT]; //Alocamos las columnas
-        
-        for(int j=0; j<EVENTCOUNT; j++)
+        this->FSMMatrix[i] = new Player*[EVENTCOUNT]; //Alocamos las columnas
+
+        for (int j = 0; j < EVENTCOUNT; j++)
         {
-            this->FSMMatrix[i][j]= FSMTempMatrix[i][j]; //copiamos celda a celda la matriz temporal
+            this->FSMMatrix[i][j] = FSMTempMatrix[i][j]; //copiamos celda a celda la matriz temporal
         }
     }
 }
@@ -28,14 +29,9 @@ FSM::FSMCycle(event_t event, gameData_t gameData)
 
 FSM::~FSM()
 {
-    for(int i=0; i<STATECOUNT; i++)
-         delete[] this->FSMMatrix[i]; //Desalocamos columnas
+    for (int i = 0; i < STATECOUNT; i++)
+        delete[] this->FSMMatrix[i]; //Desalocamos columnas
     delete[] this->FSMMatrix;
-    
-}
-     
-         
-              
-    
+
 }
 
