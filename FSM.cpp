ac3304527dfa8ec9c24afacc66ad2c9b109e767d
Rober <rcha@itba.edu.ar>
2017-10-07 19:51:25 -0300

    Hicimos el constructor con la matriz y la funcion FSMCycle.

--- FSM.cpp
+++ FSM.cpp
@@ -1,14 +1,41 @@
 #include "FSM.h"
 
-FSM::FSM()
-{
+FSM::FSM(Player* Player1, Player* Player2){
+    Player* FSMTempMatrix[STATECOUNT][EVENTCOUNT] =  //Creamos matriz temporal para luego copiar a la final 
+    {
+        //ACTION   TURN
+        {&Player1, &Player2}, //Player 1
+        {&Player2, &Player1}, //Player 2
+    };
+    
+    this->FSMMatrix = new Player**[STATECOUNT]; //Alocamos las filas de la matriz
+    for(int i=0; i<STATECOUNT; i++)
+    {
+        this->FSMMatrix[i]= new Player*[EVENTCOUNT]; //Alocamos las columnas
+        
+        for(int j=0; j<EVENTCOUNT; j++)
+        {
+            this->FSMMatrix[i][j]= FSMTempMatrix[i][j]; //copiamos celda a celda la matriz temporal
+        }
+    }
 }
 
-FSM::FSM(const FSM& orig)
+FSM::FSMCycle(event_t event, gameData_t gameData)
 {
+    currentPlayer->eventHandler(event, gameData);
+    currentPlayer = FSMMatrix[currentPlayer->getCurrentPlayer()][event];
 }
 
 FSM::~FSM()
 {
+    for(int i=0; i<STATECOUNT; i++)
+         delete[] this->FSMMatrix[i]; //Desalocamos columnas
+    delete[] this->FSMMatrix;
+    
+}
+     
+         
+              
+    
 }
 
--- FSM.h
+++ FSM.h
@@ -1,13 +1,19 @@
 #ifndef FSM_H
 #define FSM_H
 
+#include "Player.h"
+
+typedef enum {ACTION, TURN, EVENTCOUNT} event_t;
+typedef enum {P1, P2, STATECOUNT} state_t;
 class FSM {
 public:
-    FSM();
-    FSM(const FSM& orig);
+    FSM(Player*, Player*);
+    FSMCycle(event_t, gameData_t);
     virtual ~FSM();
 private:
-
+    Player* currentPlayer;
+    Player** FSMMatrix;
+    Player* FSMMatrix[][EVENTCOUNT];
 };
 
 #endif /* FSM_H */
