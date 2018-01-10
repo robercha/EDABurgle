5df90d5e9e116ccf967f8d912ac1d0c43fba41e4
Lucero Fernandez <lucfernandez@itba.edu.ar>
2018-01-10 02:08:20 -0300

    agregado todo lo que hacía falta de View.h, defines, enums de los botones/imagenes, existe y estaría completa (a excepcion de actions y chat) graphicsData_t

--- View.h
+++ View.h
@@ -18,14 +18,125 @@
 
 #define DISPLAYW 1280
 #define DISPLAYH 706
+#define V_TOTAL_PLAYERS         2
+#define V_TOTAL_GUARDS          3
+#define V_TOTAL_LOOTS           3
+#define V_TOTAL_TILES           48
+#define V_TOTAL_CRACKINGDICE    6
 
-typedef enum {
-    TEST, BUTTON_COUNT, NOBUTTON, HOME_EXIT
-} button_t;
+enum class button { //buttons != images
+    //tiles
+    A1F1, A2F1, A3F1, A4F1, B1F1, B2F1, B3F1, B4F1, C1F1, C2F1, C3F1, C4F1, D1F1, D2F1, D3F1, D4F1,
+    A1F2, A2F2, A3F2, A4F2, B1F2, B2F2, B3F2, B4F2, C1F2, C2F2, C3F2, C4F2, D1F2, D2F2, D3F2, D4F2,
+    A1F3, A2F3, A3F3, A4F3, B1F3, B2F3, B3F3, B4F3, C1F3, C2F3, C3F3, C4F3, D1F3, D2F3, D3F3, D4F3,
+    //LOOTF1, LOOTF2, LOOTF3,
+    //actions
+    MOVE, PEEK, PASS, ETC,
+
+    BUTTON_COUNT, NOBUTTON// , HOME_EXIT
+};
+
+enum class image { //buttons != images
+    //characters
+    JUICER, HACKER, ACROBAT, SPOTTER, HAWK, RAVEN, PETERMAN, GUARD,
+    //loots
+    TIARA, KITTY, PAINTING, MIRROR, KEYCARD, ISOTOPE, GEMSTONE, GOBLET, CHIHUAHUA, GOLD, GOLD2, NO_LOOT,
+    //rooms
+    ATRIUM, CAMERA, CR_FINGERPRINT, CR_LASER, CR_MOTION, DEADBOLT, DETECTOR, FINGERPRINT, FOYER, KEYPAD,
+    LABORATORY, LASER, LAVATORY, MOTION, SAFE, SECRETDOOR, SERVICEDUCT, STAIRS, THERMO, WALKWAY, ROOMBACK,
+    //tokens
+    ALARMTOKEN, STEALTHTOKEN, HACKTOKEN, CRACKEDTOKEN, DOWNSTAIRSTOKEN, KITTYTOKEN, CROWTOKEN, OPENTOKEN,
+    //patrol
+    A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4, NO_PATROL,
+    //combination numbers
+    NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5, NUMBER6,
+    //dice
+    GUARD_DIE1, GUARD_DIE2, GUARD_DIE3, GUARD_DIE4, GUARD_DIE5, GUARD_DIE6,
+    SAFE_DIE1, SAFE_DIE2, SAFE_DIE3, SAFE_DIE4, SAFE_DIE5, SAFE_DIE6,
+    IMAGE_COUNT
+};
+
+enum class character_t {
+    JUICER, HACKER, ACROBAT, SPOTTER, HAWK, RAVEN, PETERMAN, GUARD, BUTTON_COUNT
+};
+//typedef character viewCharacter_t;
+
+enum class loot_t {
+    TIARA, KITTY, PAINTING, MIRROR, KEYCARD, ISOTOPE, GEMSTONE, GOBLET, CHIHUAHUA, GOLD, NO_LOOT,
+};
+//typedef loot viewLoot_t;
+
+enum class room_t {
+    ATRIUM, CAMERA, CR_FINGERPRINT, CR_LASER, CR_MOTION, DEADBOLT, DETECTOR, FINGERPRINT, FOYER, KEYPAD,
+    LABORATORY, LASER, LAVATORY, MOTION, SAFE, SECRETDOOR, SERVICEDUCT, STAIRS, THERMO, WALKWAY, ROOMBACK
+};
+//typedef room viewRooms_t;
+
+enum class location_t {
+    A1F1, A2F1, A3F1, A4F1, B1F1, B2F1, B3F1, B4F1, C1F1, C2F1, C3F1, C4F1, D1F1, D2F1, D3F1, D4F1,
+    A1F2, A2F2, A3F2, A4F2, B1F2, B2F2, B3F2, B4F2, C1F2, C2F2, C3F2, C4F2, D1F2, D2F2, D3F2, D4F2,
+    A1F3, A2F3, A3F3, A4F3, B1F3, B2F3, B3F3, B4F3, C1F3, C2F3, C3F3, C4F3, D1F3, D2F3, D3F3, D4F3, V_NO_LOCATION
+};
+//typedef location viewLocation_t;
+
+enum class patrol_t {
+    A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4, NO_PATROL
+};
+//typedef patrol viewPatrol_t;
+
+typedef struct {
+    character_t character;
+    unsigned stealthTokens;
+    unsigned actionsLeft;
+    location_t location;
+
+} player_t;
+
+typedef struct {
+    character_t character; //es necesario? se sabe q es guardia
+    unsigned movements; //moves por turno del guardia, 6 como maximo
+    location_t location;
+    location_t guardDie; //donde esta el dado (a donde va si no suenan alarmas)
+    patrol_t patrolDeck;
+
+} guard_t;
+
+typedef struct {
+    room_t iAm; //e.g. atrium
+    bool alarm; //on-off
+    unsigned combinationNumber; //0 si es la safe, ó 1,2,3,4,5,6
+    bool crackedToken; //si salió con los cracking dice, para dibujar cracked token sobre el combination number
+    unsigned howManyHackTokens; //solo computers room (3 comp rooms)
+    bool goldBarOnTheLoose; //dónde esta la segunda gold bar q debe ser agarrada por alguno de los ladrones
+    bool kittyToken; //para kitty token
+    bool crowToken;
+    bool keypadOpenToken; //para keypad room
+    bool stairsToken;
+    unsigned howManyStealthTokens; //solo para lavatory
+
+} tile_t;
+
+typedef struct {
+    player_t players[V_TOTAL_PLAYERS];
+    guard_t guards[V_TOTAL_GUARDS];
+    tile_t tiles[V_TOTAL_TILES];
+    loot_t loots [V_TOTAL_LOOTS]; //para mostrar los loots de c/piso abajo de los floors
+    tile_t currentCardSelected; //muestra en upper-right corner toda la informacion
+    unsigned crackingDice[V_TOTAL_CRACKINGDICE];
+
+
+    //aca faltan los mensajes
+    //aca faltan las actions (botones)
+
+    bool gameWon; //solo fijarse por gameWon=true
+    bool gameLost; //solo fijarse por gameLost=true
+
+
+} graphicsData_t;
 
 typedef struct bitmap //informacion de cada boton (bitmap)
 {
-    ALLEGRO_BITMAP* image; //el archivo
+    //ALLEGRO_BITMAP* image; //el archivo
     //unsigned screen; //a que pantalla pertenece
     unsigned int x; //posicion en la pantalla
     unsigned int y;
@@ -35,26 +146,44 @@ typedef struct bitmap //informacion de cada boton (bitmap)
 
 class View {
 public:
-    View();
+
     void menuDisplay(void);
-    virtual ~View();
     unsigned getButtonX(unsigned); //se le pasa el indice del arreglo de botones
     unsigned getButtonY(unsigned);
-    unsigned getButtonW(unsigned);
     unsigned getButtonH(unsigned);
+    unsigned getButtonW(unsigned);
+    View();
+    virtual ~View();
+    //algunas de estas en private pls
+    void drawFloors();
+    void drawWalls();
+    void drawCharacters();
+    void drawStealthTokens();
+    void drawGuards();
+    void showPatrolDeck();
+    void drawBackground();
+    void drawTitle();
+    void drawActions();
+    void drawMessages();
+
+
+
 private:
+
     ALLEGRO_DISPLAY *display;
     ALLEGRO_BITMAP *background;
     ALLEGRO_EVENT_QUEUE* eventQueue;
     ALLEGRO_TIMER* timer;
     ALLEGRO_FONT *titleFont;
     ALLEGRO_FONT *textFont;
-    unsigned int backgroundWidth;
-    unsigned int backgroundHeight;
-    unsigned int width;
-    unsigned int height;
-    bitmap_t* buttons;
+    unsigned int backgroundWidth; //??
+    unsigned int backgroundHeight; //??
+    unsigned int width; //??
+    unsigned int height; //??
+    //bitmap_t* buttons;
     std::vector<bitmap_t>* buttons;
+    std::vector<ALLEGRO_BITMAP>* images;
+
 };
 
 #endif /* VIEW_H */
