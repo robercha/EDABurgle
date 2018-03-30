c5519c7cf245cbb277dfdcbfe8b97cfaaa889b09
Lucero Fernandez <lucfernandez@itba.edu.ar>
2018-03-30 17:28:48 -0300

    moveGuard realizada

--- Floor.h
+++ Floor.h
@@ -28,7 +28,7 @@ private:
     Tile* minDistance();
     void unvisitTiles();
     Guard* guard;
-    std::
+    bool compare();
     std::vector<patrol_t> patrolDeck;
     std::vector<patrol_t> trashedPatrolDeck;
     std::vector< std::vector<Tile*> > tiles;
