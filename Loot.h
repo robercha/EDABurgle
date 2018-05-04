f00af4f91e02f630f1d474c49b04f0e310dcf7bf
Lucero Fernandez <lucfernandez@itba.edu.ar>
2018-05-04 19:07:38 -0300

    Tile.h/cpp y Loot.h/cpp compilan

--- Loot.h
+++ Loot.h
@@ -1,7 +1,7 @@
 #ifndef LOOT_H
 #define LOOT_H
 
-#include "Guard.h"
+#include "Tile.h"
 
 typedef enum {
     TIARA, PERSIAN_KITTY, PAINTING, MIRROR, KEYCARD, ISOTOPE, GEMSTONE, CURSED_GOBLET, CHIHUAHUA, GOLD_BAR, LOOT_COUNT
@@ -20,7 +20,7 @@ public:
     //void giveLoot(Character* destiny);
     //void takeLoot(Character* source);
     void pickUpLoot();
-    bool virtual awakenCurse() = 0;
+    //bool virtual awakenCurse() = 0;
     loot_t getLootName();
 
     player_t getOwner() {
@@ -34,7 +34,7 @@ protected:
     bool isVisible;
     loot_t lootName;
     player_t owner;
-};
+} ;
 
 class Goblet : public Loot {
 public:
@@ -44,10 +44,10 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse();
+    //bool awakenCurse();
 private:
     bool virgin;
-};
+} ;
 
 class Kitty : public Loot { //chequear al principio del turno
 public:
@@ -57,14 +57,14 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse();
+    //bool awakenCurse();
     void setTile();
 private:
     bool rollDice(); //si es 1 o 2
     bool isThereAnAlarmTile();
     Tile* calculateRoute(); //devuelve la tile a la q deberia moverse el kitty
     Tile* currentTile; //o location_t location;
-};
+} ;
 
 class Tiara : public Loot { //chequear al final del turno
 public:
@@ -74,13 +74,14 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse();
-    void setGuard(Guard*);
+    //bool awakenCurse();
+    void setGuardLocation(location_t);      //algo asi
 private:
     bool isGuardNearby();
-    Guard* guard;
+    location_t guardLocation;
+    //Guard* guard;     //no conoce guard, deberian pasarle la location_t
 
-};
+} ;
 
 class Painting : public Loot {
 public:
@@ -90,11 +91,11 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //si devuelve true, no puede pasar
+    //bool awakenCurse(); //si devuelve true, no puede pasar
 private:
     bool onServiceDuctTile();
     bool isSecretDoorNearby();
-};
+} ;
 
 class Mirror : public Loot { //principio del turno y cuando se mueve el personaje
 public:
@@ -104,11 +105,11 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //si devuelve false, no cambia el estado de la alarma
+    //bool awakenCurse(); //si devuelve false, no cambia el estado de la alarma
 private:
     bool deactivateLaser(); //evita que se active el flag de triggered alarm
     void decreaseActions(); //si tiene >3 actions, le resta una
-};
+} ;
 
 class KeyCard : public Loot {
 public:
@@ -118,9 +119,9 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //poner en todos los safe tile el flag de poder crakear en false y si el jugador esta en esa safe tile cambiarlo a true
+    //bool awakenCurse(); //poner en todos los safe tile el flag de poder crakear en false y si el jugador esta en esa safe tile cambiarlo a true
 private:
-};
+} ;
 
 class Isotope : public Loot {
 public:
@@ -130,10 +131,10 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //dispara alarma de thermo
+    //bool awakenCurse(); //dispara alarma de thermo
 private:
     bool onThermoTile(); //tal vez no es necesario
-};
+} ;
 
 class Gemstone : public Loot {
 public:
@@ -143,14 +144,14 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //chequeamos que prevTile y currTile sean diferentes, y que onSameTile devuelva true
+    //bool awakenCurse(); //chequeamos que prevTile y currTile sean diferentes, y que onSameTile devuelva true
     void setPartner(/*Character**/); //solo una vez
     void setPrevTile(Tile*); //solo se setea cuando agarra el loot
 private:
     bool onSameTileAsPartner();
     //Character* partner;
     Tile* previousTile;
-};
+} ;
 
 class Chihuahua : public Loot { //chequear al principio del turno
 public:
@@ -160,10 +161,10 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //triggerea alarma de cualquier currentTile
+    //bool awakenCurse(); //triggerea alarma de cualquier currentTile
 private:
     bool rollDice(); //si es 6
-};
+} ;
 
 class GoldBar : public Loot { //quien lleve un goldbar debe ser diferente a quien lleve el otro
 public:
@@ -173,12 +174,12 @@ public:
         isVisible = false;
         owner = NO_PLAYER;
     };
-    bool awakenCurse(); //clonarse y crear GoldBar2
+    //bool awakenCurse(); //clonarse y crear GoldBar2
     //   Loot getGoldBar();
 private:
     //  Loot goldBar2;
 
-};
+} ;
 
 #endif /* LOOT_H */
 
