3c3187a122b132ab95a0d10be24c8c7dcb2d3db5
nachograyeb <37944502+nachograyeb@users.noreply.github.com>
2018-05-05 19:15:14 -0300

    Update Character.cpp
    Completé la mayoría de los characters. Faltan un par.

--- Character.cpp
+++ Character.cpp
@@ -38,7 +38,7 @@ unsigned Character::getStealthTokensQty()
     return stealthTokens;
 }
 
-location_t Character::getLocation()
+location_t Character::getLocation() 
 {
     return currentTile->getCurrentLocation();
 }
@@ -107,6 +107,73 @@ Peterman::Peterman()
     actions = INIT_ACTIONS;
 }
 
+bool Hacker::isFML()
+{
+	bool isItFML = false;
+	tileType_t tileType = currentTile->getTileType();
+	if((tileType == FINGERPRINT)||(tileType == MOTION)||(tileType == LASER))
+		isItFML = true;
+	
+	return isItFML;
+}
+
+bool Hacker::hiddenTalent()
+{
+	bool hTalent = false;
+	if(isFML())
+	{
+		currentTile->deactivateAlarm();
+		hTalent = true;
+	}
+	
+	return hTalent;
+}
+
+void Hacker::setPartnerTile(Tile* pTile)
+{
+	partnerTile = pTile;
+}
+
+bool Hacker::isPartnerOnSameTile()
+{
+	bool same = false;
+	if(partnerTile->getCurrentLocation() == this->getLocation())
+		same = true;
+	
+	return same;
+}
+bool Acrobat::actionCount()
+{
+	bool isItZero = false;
+	if(actions == 0)
+		isItZero = true;
+	
+	return isItZero;
+}
+
+bool Acrobat::hiddenTalent(Floor* currFloor)
+{
+	bool hTalent = true; //da true si no lo agarra el guardia; false en caso contrario
+	if((actionCount)&&(isGuardOnCurrTile(currFloor)))
+	{
+		hTalent = false;
+		stealthTokens--;
+	}
+		
+	
+	return hTalent;
+}
+
+bool Acrobat::isGuardOnCurrTile(Floor* currFloor)
+{
+	bool isGuardHere = false;
+	location_t guardLocation = currFloor->guard->getLocation();
+	if(guardLocation == this->getLocation())
+		isGuardHere = true;
+	
+	return isGuardHere;
+}
+
 bool Hawk::canIUseThisTile(location_t selectedTile, tileInfo_t* tileInfo)
 {
     bool isTileValid = false;
@@ -125,6 +192,28 @@ bool Hawk::canIUseThisTile(location_t selectedTile, tileInfo_t* tileInfo)
 
     return isTileValid;
 }
+
+bool Hawk::hiddenTalent(location_t selectedTile, tileInfo_t* tileInfo)
+{
+	bool canIUseHiddenTalent = false;
+	if(canIUseThisTile(selectedTile,tileInfo)&&(tileInfo->hawkWall))
+	{
+		peek(tileInfo->tile);
+		addAction();
+		canIUseHiddenTalent = true;
+	}
+	
+	return canIUseHiddenTalent;
+	
+	
+}
+
+void Hawk::addAction()
+{
+	actions++;
+}
+
+
 
 bool Raven::canIUseThisTile(location_t selectedTile, tileInfo_t* tileInfo)
 {
@@ -147,6 +236,18 @@ bool Raven::canIUseThisTile(location_t selectedTile, tileInfo_t* tileInfo)
     return isTileValid;
 }
 
+bool Raven::hiddenTalent(location_t selectedTile, tileInfo_t* tileInfo)
+{
+	bool canIUseHiddenTalent = false;
+	
+	if(canIUseThisTile(selectedTile, tileInfo))
+		placeCrowToken(tileInfo->tile);
+	
+	return canIUseHiddenTalent;
+		
+}
+
+
 void Raven::placeCrowToken(Tile* tile)
 {
     tile->setToken(CROWTOKEN, NULL);
@@ -155,4 +256,25 @@ void Raven::placeCrowToken(Tile* tile)
 void Juicer::placeExtraAlarm(Tile* tile)
 {
     tile->setJuicerAlarm();
+}
+
+bool Peterman::hiddenTalent()
+{
+	bool hTalent = false;
+	if(whereAmI() == SAFE || whereAmI() == KEYPAD)
+	{
+		throwAdditionalDice();
+		hTalent = true;
+	}
+	
+	return hTalent;
+	
+}
+
+unsigned Peterman::throwAdditionalDice()
+{
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	unsigned result = rand_r(seed % 6 + 1);
+	
+	return result;
 }
