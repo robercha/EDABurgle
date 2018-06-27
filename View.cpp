6f77a706d09bb5c6293e2c9de79551d8f6c451ee
Lucero Fernandez <lucfernandez@itba.edu.ar>
2018-06-27 19:08:05 -0300

    agregados grises a las actions

--- View.cpp
+++ View.cpp
@@ -312,8 +312,10 @@ View::drawCardInfo()
             drawSelectedTileTokens();                                       //los tokens que tiene
         }
         else if (graphicsData->tiles[(int) graphicsData->currentCardSelected].iAm == roomV_t::V_ROOMBACK)
+        {
             drawTileSelectedInfo();                                         //dibuja la carta
-        writeSelectedTileInfo();                                        //qué carta es, su ubicacion
+            writeSelectedTileInfo();                                        //qué carta es, su ubicacion
+        }
 
     }
     else
